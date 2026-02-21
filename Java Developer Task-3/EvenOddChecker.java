70bff7a576527cb2ef27a31fc3b64297d6575e45
Sakshi Jadhav <sakshijadhav0131@gmail.com>
2026-02-21 14:48:00 +0530

    task 3

---
+++ Java Developer Task-3/EvenOddChecker.java
@@ -0,0 +1,23 @@
+import java.util.Scanner; 
+ 
+public class EvenOddChecker { 
+    public static void main(String[] args) { 
+        // Static value version 
+        int staticNumber = 27; 
+        checkAndPrint(staticNumber); 
+         
+        // Interactive version 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.print("\nEnter an integer: "); 
+        int userNumber = scanner.nextInt(); 
+        checkAndPrint(userNumber); 
+         
+        scanner.close(); 
+    } 
+     
+    // Modular method demonstrating ternary operator 
+    private static void checkAndPrint(int number) { 
+        String result = (number % 2 == 0) ? "Even" : "Odd"; 
+        System.out.printf("%d is %s\n", number, result); 
+    } 
+}
